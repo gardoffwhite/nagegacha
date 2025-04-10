9ff4fa25bf2b367964b3c3c1a504efa22acdbaee
gardoffwhite <gardoffwhite@gmail.com>
2025-04-11 03:51:25 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-๐import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbzib6C9lGk23Zemy9f0Vj78E5eK8-TQBIaZEGPE5l0FT2Kc0-vDbdfK5xsRG58qmseGsA/exec';
